8eecf96641d4320e2a06acbb9d3d1c327649d1fe
Gustavo Finau Pellanda <129123498+GustavoPellanda@users.noreply.github.com>
2023-04-28 03:09:48 -0300

    criação do registro de produtos

--- cliente.py
+++ cliente.py
@@ -13,10 +13,35 @@ class Cliente_Leilao:
         assinatura = hmac.new(cliente.chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
         cliente.ref_servidor.verificar_assinatura(cliente.nome, mensagem, assinatura)
 
-    def interface(cliente):
+    def cadastrar_produto(cliente):
+        codigo = input("Código do produto: ")
+        nome = input("Nome do produto: ")
+        descricao = input("Descrição do produto: ")
+        preco_inicial = input("Preço inicial: ")
+        tempo_final = int(input("Tempo final (em segundos): "))
+
+        mensagem = str(codigo) + nome + descricao + str(preco_inicial) + str(tempo_final)
+        assinatura = hmac.new(cliente.chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
+
+        cliente.ref_servidor.registrar_produto(codigo, nome, descricao, preco_inicial, tempo_final, cliente.nome, assinatura)
+
+        print("Produto cadastrado com sucesso!")
+
+    def menu(cliente):
         while True:
-            mensagem = input("Enviar mensagem ao servidor: ")
-            cliente.enviar_mensagem(mensagem)
+            print("Selecione uma opção:")
+            print("1 - Cadastrar Produto")
+            print("2 - Consultar Leilões")
+            opcao = input("Opção: ")
+            if opcao == "1":
+                cliente.cadastrar_produto()
+            elif opcao == "2":
+                cliente.consultar_leiloes()
+            else:
+                print("Opção inválida. Tente novamente.")
+    
+    def consultar_leiloes(cliente):
+        print("Consultando leilões...")
 
 def main():
     # Registra Cliente_Leilao no Servidor_Leilao
@@ -25,8 +50,7 @@ def main():
     # Envia a chave recebida do servidor para o objeto de Cliente_Leilao
     cliente = Cliente_Leilao("Cliente01", chave)
 
-    # Mantém Cliente_Leilao ativo
-    cliente.interface()
+    cliente.menu()
 
 if __name__ == '__main__':
     main()
