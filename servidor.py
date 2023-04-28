7dc2531dab7d464a3b5795c284a4e7770c9296d8
Gustavo Finau Pellanda <129123498+GustavoPellanda@users.noreply.github.com>
2023-04-28 03:08:02 -0300

    criação do registro de produtos

--- servidor.py
+++ servidor.py
@@ -1,39 +1,65 @@
 import Pyro5.api
 import hashlib
 import hmac
-import secrets
+import time
 
 @Pyro5.api.expose
 class Servidor_Leilao:
-    def __init__(serv):
-        serv.dic_clientes = {}
+    def __init__(servidor):
+        servidor.clientes = {}
 
-    def registrar_cliente(serv, nome_cliente):
-        chave = secrets.token_hex(16)  # gerador de chaves
-        serv.dic_clientes[nome_cliente] = chave
-
-        print(f"Novo cliente registrado: {nome_cliente}")
+    def registrar_cliente(servidor, nome_cliente):
+        chave = hashlib.sha256(str(time.time()).encode('utf-8')).hexdigest()
+        servidor.clientes[nome_cliente] = chave
         return chave
 
-    def verificar_assinatura(serv, nome_cliente, mensagem, assinatura):
-        chave = serv.dic_clientes.get(nome_cliente)
-        if not chave:
-            raise ValueError(f"Assinatura do cliente '{nome_cliente}' não encontrada.")
-        if assinatura != hmac.new(chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest():
-            raise ValueError(f"Assinatura do cliente '{nome_cliente}' é inválida.")
-        print(f"Assinatura do cliente '{nome_cliente}' verificada!")
+    @Pyro5.api.expose
+    def verificar_assinatura(servidor, nome_cliente, mensagem, assinatura):
+        chave = servidor.clientes[nome_cliente]
+        expected = hmac.new(chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
+        if not hmac.compare_digest(expected, assinatura):
+            raise ValueError("Assinatura inválida!")
 
-        print(f"Mensagem de '{nome_cliente}' recebida: {mensagem}")
+    @Pyro5.api.expose
+    def registrar_produto(servidor, codigo, nome, descricao, preco_inicial, tempo_final, nome_cliente, assinatura):
+        mensagem = str(codigo) + nome + descricao + str(preco_inicial) + str(tempo_final)
+        servidor.verificar_assinatura(nome_cliente, mensagem, assinatura)
+        print("Produto registrado com sucesso!")
+
+@Pyro5.api.expose
+class Produto:
+    def __init__(prod, codigo, nome, descricao, preco_inicial, tempo_final, servidor):
+        prod.codigo = codigo
+        prod.nome = nome
+        prod.descricao = descricao
+        prod.preco_inicial = preco_inicial
+        prod.tempo_final = tempo_final
+        prod.servidor = servidor
+
+    @Pyro5.api.expose
+    def registrar_produto(servidor, codigo, nome, descricao, preco_inicial, tempo_final, nome_cliente, assinatura):
+        mensagem = str(codigo) + nome + descricao + str(preco_inicial) + str(tempo_final)
+        chave_cliente = servidor.clientes_registrados.get(nome_cliente)
+        if chave_cliente is None:
+            raise ValueError("Cliente não registrado!")
+        assinatura_valida = hmac.compare_digest(assinatura, hmac.new(chave_cliente.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest())
+        if not assinatura_valida:
+            raise ValueError("Assinatura inválida!")
+        
+        produto = Produto(codigo, nome, descricao, preco_inicial, tempo_final, nome_cliente)
+        servidor.produtos.append(produto)
 
 def main():
-    # Registro de uma instância de Servidor_Leilao no Daemon
+    # Registro de uma instância de Servidor_Leilao no Daemon:
     daemon = Pyro5.api.Daemon()
-    uri = daemon.register(Servidor_Leilao())
+    servidor = Servidor_Leilao()
 
-    # Localiza o servidor de nomes
+    # Registro dos métodos no Name Server:
     ns = Pyro5.api.locate_ns()
-    # Registra Servidor_Leilao no servidor de nomes
+    uri = daemon.register(servidor)
     ns.register("Servidor_Leilao", uri)
+    prod_uri = daemon.register(Produto)
+    ns.register("Produto", prod_uri)
 
     print("Servidor do leilão registrado. Pronto para uso!")
     daemon.requestLoop()
