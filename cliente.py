b0fd18994358024ca1519e5abdfb0fed43dba8ef
Gustavo Finau Pellanda <129123498+GustavoPellanda@users.noreply.github.com>
2023-04-30 18:22:01 -0300

    desfazendo burrices e iniciando a consulta

--- cliente.py
+++ cliente.py
@@ -7,11 +7,7 @@ class Cliente_Leilao:
     def __init__(cliente, nome, chave):
         cliente.nome = nome
         cliente.chave = chave
-        cliente.ref_servidor = Pyro5.api.Proxy("PYRONAME:Servidor_Leilao")
-
-    def enviar_mensagem(cliente, mensagem):
-        assinatura = hmac.new(cliente.chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
-        cliente.ref_servidor.verificar_assinatura(cliente.nome, mensagem, assinatura)
+        cliente.servidor = Pyro5.api.Proxy("PYRONAME:Servidor_Leilao")
 
     def cadastrar_produto(cliente):
         codigo = input("Código do produto: ")
@@ -23,33 +19,30 @@ class Cliente_Leilao:
         mensagem = str(codigo) + nome + descricao + str(preco_inicial) + str(tempo_final)
         assinatura = hmac.new(cliente.chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
 
-        cliente.ref_servidor.registrar_produto(codigo, nome, descricao, preco_inicial, tempo_final, cliente.nome, assinatura)
+        cliente.servidor.registrar_produto(codigo, nome, descricao, preco_inicial, tempo_final, cliente.nome, assinatura)
 
-        print("Produto cadastrado com sucesso!")
+        print(f"Produto '{nome}' registrado com sucesso!")
 
     def menu(cliente):
         while True:
-            print("Selecione uma opção:")
+            print("\nSelecione uma opção:")
             print("1 - Cadastrar Produto")
             print("2 - Consultar Leilões")
             opcao = input("Opção: ")
+
             if opcao == "1":
                 cliente.cadastrar_produto()
             elif opcao == "2":
-                cliente.consultar_leiloes()
+                cliente.servidor.listar_produtos()
             else:
                 print("Opção inválida. Tente novamente.")
-    
-    def consultar_leiloes(cliente):
-        print("Consultando leilões...")
-
+        
 def main():
     # Registra Cliente_Leilao no Servidor_Leilao
     servidor = Pyro5.api.Proxy("PYRONAME:Servidor_Leilao")
     chave = servidor.registrar_cliente("Cliente01")
     # Envia a chave recebida do servidor para o objeto de Cliente_Leilao
     cliente = Cliente_Leilao("Cliente01", chave)
-
     cliente.menu()
 
 if __name__ == '__main__':
