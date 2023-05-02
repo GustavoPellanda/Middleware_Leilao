4a6b2bb3ac640182984cd42892d5545010bdc645
Gustavo Finau Pellanda <129123498+GustavoPellanda@users.noreply.github.com>
2023-05-02 03:15:53 -0300

    preços agora são atualizados

--- cliente.py
+++ cliente.py
@@ -8,21 +8,21 @@ class Cliente_Leilao:
         cliente.nome = nome
         cliente.chave = chave
         cliente.servidor = Pyro5.api.Proxy("PYRONAME:Servidor_Leilao")
-        cliente.produtos = []
+        cliente.produtos = [] # Será utilizado para listar os produtos
 
     def cadastrar_produto(cliente):
         codigo = input("Código do produto: ")
         nome = input("Nome do produto: ")
         descricao = input("Descrição do produto: ")
-        preco_inicial = input("Preço inicial: ")
-        tempo_final = int(input("Tempo final: "))
+        preco_inicial = int(input("Preço inicial: "))
+        tempo_final = int(input("Tempo final em horas: "))
 
         mensagem = str(codigo) + nome + descricao + str(preco_inicial) + str(tempo_final)
         assinatura = hmac.new(cliente.chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
 
         cliente.servidor.registrar_produto(codigo, nome, descricao, preco_inicial, tempo_final, cliente.nome, assinatura)
 
-        print(f"Produto '{nome}' registrado com sucesso com prazo final de {tempo_final} horas.")
+        print(f"Produto '{nome}' registrado com prazo final de {tempo_final} horas e preço inicial de R${preco_inicial:.2f}")
 
     def listar_produtos(cliente):
         cliente.produtos = cliente.servidor.obter_produtos()
@@ -35,9 +35,10 @@ class Cliente_Leilao:
             print(f"Código: {produto['codigo']}")
             print(f"Nome: {produto['nome']}")
             print(f"Descrição: {produto['descricao']}")
-            print(f"Preço Inicial: {produto['preco_inicial']}")
-            print(f"Prazo Final: {produto['prazo_final']}")
-            print(f"Tempo Restante: {produto['tempo_restante']:.2f} segundos")
+            print(f"Preço Inicial: {produto['preco_inicial']:.2f}")
+            print(f"Preço Atual: R${produto['preco_atual']:.2f}")
+            print(f"Tempo Restante: {produto['prazo_final']:.2f}")
+            print(f"Prazo Final: {produto['tempo_restante']:.2f} segundos")
             print(f"Nome do Cliente: {produto['nome_cliente']}")
         
     def menu(cliente):
@@ -59,15 +60,15 @@ class Cliente_Leilao:
 
     def enviar_lance(cliente):
         codigo = input("Código do produto: ")
-        lance = input("Valor do lance: ")
+        lance = int(input("Valor do lance: "))
 
-        mensagem = str(codigo) + str(lance)
+        mensagem = str(codigo) + str(int(lance))
         assinatura = hmac.new(cliente.chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
 
         cliente.servidor.fazer_lance(codigo, lance, cliente.nome, assinatura)
 
-        print(f"Lance de {lance} enviado ao produto de código {codigo}.")
-                
+        print(f"Lance de R${lance:.2f} enviado ao produto de código {codigo}.")
+
 def main():
     # Registra Cliente_Leilao no Servidor_Leilao
     servidor = Pyro5.api.Proxy("PYRONAME:Servidor_Leilao")
