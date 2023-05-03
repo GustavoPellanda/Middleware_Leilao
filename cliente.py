6caf085ecda684b19ac49d7dbdad63097e6e022b
Gustavo Finau Pellanda <129123498+GustavoPellanda@users.noreply.github.com>
2023-05-03 02:44:26 -0300

    atualização dos preços refatorada

--- cliente.py
+++ cliente.py
@@ -4,13 +4,13 @@ import hmac
 
 @Pyro5.api.expose
 class Cliente_Leilao:
-    def __init__(cliente, nome, chave):
-        cliente.nome = nome
-        cliente.chave = chave
-        cliente.servidor = Pyro5.api.Proxy("PYRONAME:Servidor_Leilao")
-        cliente.produtos = [] # Será utilizado para listar os produtos
+    def __init__(self, nome, chave):
+        self.nome = nome
+        self.chave = chave
+        self.servidor = Pyro5.api.Proxy("PYRONAME:Servidor_Leilao")
+        self.produtos = [] # Será utilizado para listar os produtos
 
-    def cadastrar_produto(cliente):
+    def cadastrar_produto(self):
         codigo = input("Código do produto: ")
         nome = input("Nome do produto: ")
         descricao = input("Descrição do produto: ")
@@ -18,43 +18,41 @@ class Cliente_Leilao:
         tempo_final = int(input("Tempo final em horas: "))
 
         mensagem = str(codigo) + nome + descricao + str(preco_inicial) + str(tempo_final)
-        assinatura = hmac.new(cliente.chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
+        assinatura = hmac.new(self.chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
 
-        cliente.servidor.registrar_produto(codigo, nome, descricao, preco_inicial, tempo_final, cliente.nome, assinatura)
+        self.servidor.registrar_produto(codigo, nome, descricao, preco_inicial, tempo_final, self.nome, assinatura)
 
         print(f"Produto '{nome}' registrado com prazo final de {tempo_final} horas e preço inicial de R${preco_inicial:.2f}")
 
-    def listar_produtos(cliente):
-        cliente.produtos = cliente.servidor.obter_produtos()
-        if cliente.produtos == "Nenhum produto cadastrado":
-            print(cliente.produtos)
+    def listar_produtos(self):
+        self.produtos = self.servidor.obter_produtos()
+        if self.produtos == "Nenhum produto cadastrado":
+            print(self.produtos)
             return
         
-        for produto in cliente.produtos:
+        for produto in self.produtos:
             print()
             print(f"Código: {produto['codigo']}")
             print(f"Nome: {produto['nome']}")
             print(f"Descrição: {produto['descricao']}")
-            print(f"Preço Inicial: {produto['preco_inicial']:.2f}")
+            print(f"Preço Inicial: R${produto['preco_inicial']:.2f}")
             print(f"Preço Atual: R${produto['preco_atual']:.2f}")
-            print(f"Tempo Restante: {produto['prazo_final']:.2f}")
-            print(f"Prazo Final: {produto['tempo_restante']:.2f} segundos")
             print(f"Nome do Cliente: {produto['nome_cliente']}")
 
-    def enviar_lance(cliente):
+    def enviar_lance(self):
         codigo = input("Código do produto: ")
         lance = int(input("Valor do lance: "))
 
         mensagem = str(codigo) + str(int(lance))
-        assinatura = hmac.new(cliente.chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
+        assinatura = hmac.new(self.chave.encode('utf-8'), mensagem.encode('utf-8'), hashlib.sha256).hexdigest()
 
         # Verifica se o lance foi aprovado:
-        if not cliente.servidor.fazer_lance(codigo, lance, cliente.nome, assinatura):
+        if not self.servidor.fazer_lance(codigo, lance, self.nome, assinatura):
             print(f"Lance de R${lance:.2f} não supera lance anterior no produto de código {codigo}.")
             return
         print(f"Lance de R${lance:.2f} enviado ao produto de código {codigo}.")
         
-    def menu(cliente):
+    def menu(self):
         while True:
             print("\nSelecione uma opção:")
             print("1 - Cadastrar Produto")
@@ -63,11 +61,11 @@ class Cliente_Leilao:
             opcao = input("Opção: ")
 
             if opcao == "1":
-                cliente.cadastrar_produto()
+                self.cadastrar_produto()
             elif opcao == "2":
-                cliente.listar_produtos()
+                self.listar_produtos()
             elif opcao == "3":
-                cliente.enviar_lance()
+                self.enviar_lance()
             else:
                 print("Opção inválida. Tente novamente.")
 
